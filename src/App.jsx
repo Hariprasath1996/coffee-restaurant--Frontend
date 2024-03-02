cc5458b7da110abdc4ec1ad004d026613f1a80a2
HariPrasath1996 <hasathphy96@gmail.com>
2024-03-02 11:57:36 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 const App= () => {
   return ( 
-    <h1>
-app
-    </h1>
+  <nav>
+    <div>
+      <img src="/assets/coffee-logo.jpg" alt="logo" />
+    </div>
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+      <li>
+        <a href="#">Cart</a>
+      </li>
+      <li>
+        <a href="#">Log-In</a>
+      </li>
+    </ul>
+  </nav>
 );
 }
 
