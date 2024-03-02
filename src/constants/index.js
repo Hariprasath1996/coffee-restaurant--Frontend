e652e9fa1ab53626e776688ff6b0b70146245a75
HariPrasath1996 <hasathphy96@gmail.com>
2024-03-02 11:14:27 +0530

    create data for items

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,11 @@
 const categories = [
 {
     id : 1 ,
-    title : 'Coffee',
+    title : 'HotCoffee',
+    imageUrl : ''
+},{
+    id : 2 ,
+    title : 'ColdCoffee',
     imageUrl : ''
 }
 ]
@@ -10,12 +14,36 @@ const ShopData = [
     {
         title : 'HotCoffee',
         items : [
+{id : 1,
+name : '',
+imageUrl:'',
+price : 25},
+{id : 2,
+    name : '',
+    imageUrl:'',
+    price : 55},
+    {id : 3,
+        name : '',
+        imageUrl:'',
+        price : 125}
 
         ]
     },
     {
         title : 'ColdCoffee',
         items :[
+            {id : 1,
+                name : '',
+                imageUrl:'',
+                price : 125},
+                {id : 2,
+                    name : '',
+                    imageUrl:'',
+                    price : 145},
+                    {id :3 ,
+                        name : '',
+                        imageUrl:'',
+                        price : 215}
 
         ]
     }
