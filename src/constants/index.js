97558cefd936bffee51bda017b48557b1e48c2f8
HariPrasath1996 <hasathphy96@gmail.com>
2024-03-02 11:33:45 +0530

    add images

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,29 +2,30 @@ const categories = [
 {
     id : 1 ,
     title : 'HotCoffee',
-    imageUrl : ''
+    imageUrl : 'https://t4.ftcdn.net/jpg/05/68/26/65/360_F_568266596_dD05LhRcOQvR9n3GeFidtEtjKssMXCKL.jpg',
 },{
     id : 2 ,
     title : 'ColdCoffee',
-    imageUrl : ''
+    imageUrl : 'https://realfood.tesco.com/media/images/RFO-1400x919-IcedCoffee-76221116-2565-4103-9899-89571028018e-0-1400x919.jpg',
 }
-]
+];
+
 
 const ShopData = [
     {
         title : 'HotCoffee',
         items : [
 {id : 1,
-name : '',
-imageUrl:'',
+name : 'hot-milky',
+imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKfbmlfLb8JPBAZMOOZcBNmnXlb32h2a-uaJ8F7Wy-RpemVSeYHEVz3bvoxXUfJ-Ss_S4&usqp=CAU',
 price : 25},
 {id : 2,
-    name : '',
-    imageUrl:'',
+    name : 'specials-hot-coffee',
+    imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4g1AvBS-ygfqtyUBLSjq7tsRTi-mDGhu-PQ&usqp=CAU',
     price : 55},
     {id : 3,
-        name : '',
-        imageUrl:'',
+        name : 'triple-special-coffee',
+        imageUrl:'https://cff2.earth.com/uploads/2018/11/01142049/Hot-or-iced-Hot-coffee-has-more-antioxidants-than-cold-brew.jpg',
         price : 125}
 
         ]
@@ -32,17 +33,17 @@ price : 25},
     {
         title : 'ColdCoffee',
         items :[
-            {id : 1,
-                name : '',
-                imageUrl:'',
+            {id : 4,
+                name : 'ice-bar-coffee',
+                imageUrl:'https://lorcoffee.com/cdn/shop/articles/Dark-Chocolate-Ice-Coffee-with-Provocateur-exc.jpg?v=1675806078',
                 price : 125},
-                {id : 2,
-                    name : '',
-                    imageUrl:'',
+                {id : 5,
+                    name : 'cold-coffee',
+                    imageUrl:'https://www.southernliving.com/thmb/wcI2A-i0qEAMQNdimSFIAFGoOwg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Coffee_Beauty_580-33e72cc4f3c84e51a7a5a8ccb0447cd8.jpg',
                     price : 145},
-                    {id :3 ,
-                        name : '',
-                        imageUrl:'',
+                    {id :6,
+                        name : 'coffee with ice bars',
+                        imageUrl:'https://outlets.roastea.in/image/catalog/BLOG%2009.jpg',
                         price : 215}
 
         ]
