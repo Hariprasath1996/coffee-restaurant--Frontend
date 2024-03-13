b5a2696be33c0258db94d9105de33ff2ba674135
HariPrasath1996 <hasathphy96@gmail.com>
2024-03-13 09:41:08 +0530

    created nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 const App= () => {
   return ( 
   <nav className="nav-bar flex flex-row w-screen h-auto justify-center align-center ">
-    <div>
-      <img className="img-container " src="/assets/coffee-logo.jpg" alt="logo" />
+    <div className="img-container w-[50rem] h-auto flex flex-row justify-start items-start gap-8" >
+      <img className="w-[4rem] rounded-xl  h- auto justify-start items-start  " src="/assets/coffee-logo.jpg" alt="logo" />
+      <h1 className=" h-auto flex flex-row justify-center items-center mt-6 font-bold text-3xl ">Masaala coffee Bar</h1>
     </div>
-    <ul>
+    <ul className="sections-list w-[50rem] h-auto flex flex-row justify-start items-start gap-10 mt-8 font-bold text-2xl">
       <li>
-        <a className=" " href="#">Home</a>
+        <a href="#">Home</a>
       </li>
       <li>
         <a href="#">Contact</a>
@@ -14,7 +15,7 @@ const App= () => {
       <li>
         <a href="#">Cart</a>
       </li>
-      <li>
+      <li className=" w-[50rem] h-auto flex flex-row justify-end items-end gap-20   ">
         <a href="#">Log-In</a>
       </li>
     </ul>
